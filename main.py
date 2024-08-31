5559d4a4b6af4fcf60b2c72501812f15ab91cb8a
Tugrul Arslan <tugrul.arslan@gmx.de>
2024-08-31 10:38:23 +0200

    implement the main canava functionality

--- main.py
+++ main.py
@@ -15,3 +15,23 @@ icon = ImageTk.PhotoImage(icon)
 root.iconphoto(False, icon)
 
 root.title("QR Code Generator")
+
+
+canvas = Canvas(root, width=400, height=600)
+canvas.pack()
+
+app_label = Label(root, text="QR Code Generator", fg="blue", font=("Arial", 30))
+canvas.create_window(200, 50, window=app_label)
+
+name_label = Label(root, text="Link name")
+link_label = Label(root, text="Link URL")
+canvas.create_window(200, 100, window=name_label)
+canvas.create_window(200, 160, window=link_label)
+
+name_entry = Entry(root)
+link_entry = Entry(root)
+canvas.create_window(200, 130, window=name_entry)
+canvas.create_window(200, 180, window=link_entry)
+
+button = Button(text="Generate QR code", command=generate_qrcode)
+canvas.create_window(200, 230, window=button)
